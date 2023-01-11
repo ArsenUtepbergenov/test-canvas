687bbf80acc23b1411c4d58bdaaf05c2c479c090
ArsenUtepbergenov <arsen.utepbergenov@yandex.ru>
2023-01-11 15:10:39 +0100

    p5

--- js/animation.js
+++
@@ -1,6 +0,0 @@
-export function createAnimation(frames, frameLen) {
-  return function resolveFrame(distance) {
-    const frameIndex = Math.floor(distance / frameLen) % frames.length
-    return frames[frameIndex]
-  }
-}
